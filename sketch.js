2d99099e9891b5847ea55259894b05e95be0089d
Ojaswa Rajput <69306443+OjaswaRajput@users.noreply.github.com>
2021-10-09 11:33:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,68 @@
+// Daniel Shiffman
+// http://youtube.com/thecodingtrain
+// http://codingtra.in
+
+// Coding Challenge #115: Snake Game Redux
+// https://youtu.be/OMoVcohRgZA
+
+let snake;
+let rez = 20;
+let food;
+let w;
+let h;
+
+var jumpSound
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  w = floor(width / rez);
+  h = floor(height / rez);
+  frameRate(5);
+  snake = new Snake();
+  foodLocation();
+}
+
+function foodLocation() {
+  let x = floor(random(w));
+  let y = floor(random(h));
+  food = createVector(x, y);
+
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    snake.setDir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+    snake.setDir(1, 0);
+  } else if (keyCode === DOWN_ARROW) {
+    snake.setDir(0, 1);
+  } else if (keyCode === UP_ARROW) {
+    snake.setDir(0, -1);
+  } 
+
+  function preload(){
+  jumpSound = loadSound("jump.mp3");
+  }
+
+}
+
+function draw() {
+  scale(rez);
+  background("green");
+  if (snake.eat(food)) {
+    foodLocation();
+  }
+  snake.update();
+  snake.show();
+
+
+  if (snake.endGame()) {
+    print("END GAME");
+    background(255, 0, 0);
+    noLoop();
+  }
+
+  noStroke();
+  fill(255, 0, 0);
+  rect(food.x, food.y, 1, 1);
+}
